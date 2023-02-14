c9f48b65ff55debd32f69575f11fd7c4bb63a06d
anakon73 <nazarkinah73@gmail.com>
2023-02-14 16:51:33 +0200

    chore: tailwind init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,20 @@ module.exports = {
     '@antfu',
     'plugin:react/all',
     'plugin:react/jsx-runtime',
+    'plugin:tailwindcss/recommended',
+    'plugin:tailwindcss/recommended',
   ],
   plugins: [
     'react',
   ],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'react/jsx-no-literals': [0, { noStrings: true, allowedStrings: ['allowed'], ignoreProps: false, noAttributeStrings: true }],
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.js'],
+      parser: '@typescript-eslint/parser',
+    },
+  ],
 }
