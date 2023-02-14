9c5d867b880fe15f7046325d36fba9ab5cda216f
anakon73 <nazarkinah73@gmail.com>
2023-02-14 23:17:15 +0200

    chore: storybook init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,20 @@
-process.env.ESLINT_TSCONFIG = 'tsconfig.json'
-
+process.env.ESLINT_TSCONFIG = 'tsconfig.json';
 module.exports = {
-  extends: [
-    '@antfu',
-    'plugin:react/all',
-    'plugin:react/jsx-runtime',
-    'plugin:tailwindcss/recommended',
-    'plugin:tailwindcss/recommended',
-  ],
-  plugins: [
-    'react',
-  ],
+  extends: ['@antfu', 'plugin:react/all', 'plugin:react/jsx-runtime', 'plugin:tailwindcss/recommended', 'plugin:tailwindcss/recommended', 'plugin:storybook/recommended'],
+  plugins: ['react'],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
-    'react/jsx-no-literals': [0, { noStrings: true, allowedStrings: ['allowed'], ignoreProps: false, noAttributeStrings: true }],
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.ts', '.tsx']
+    }],
+    'react/jsx-no-literals': [0, {
+      noStrings: true,
+      allowedStrings: ['allowed'],
+      ignoreProps: false,
+      noAttributeStrings: true
+    }]
   },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx', '*.js'],
-      parser: '@typescript-eslint/parser',
-    },
-  ],
-}
+  overrides: [{
+    files: ['*.ts', '*.tsx', '*.js'],
+    parser: '@typescript-eslint/parser'
+  }]
+};
