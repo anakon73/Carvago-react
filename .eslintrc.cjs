b64439d85c14b61db917910200e7a6f34582fb20
anakon73 <nazarkinah73@gmail.com>
2023-02-14 23:38:42 +0200

    chore: heroicons install

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,21 @@
-process.env.ESLINT_TSCONFIG = 'tsconfig.json';
+process.env.ESLINT_TSCONFIG = 'tsconfig.json'
 module.exports = {
   extends: ['@antfu', 'plugin:react/all', 'plugin:react/jsx-runtime', 'plugin:tailwindcss/recommended', 'plugin:tailwindcss/recommended', 'plugin:storybook/recommended'],
   plugins: ['react'],
   rules: {
     'react/jsx-filename-extension': [1, {
-      extensions: ['.ts', '.tsx']
+      extensions: ['.ts', '.tsx'],
     }],
     'react/jsx-no-literals': [0, {
       noStrings: true,
       allowedStrings: ['allowed'],
       ignoreProps: false,
-      noAttributeStrings: true
-    }]
+      noAttributeStrings: true,
+    }],
+    'react/forbid-component-props': [1, { forbid: ['false'] }],
   },
   overrides: [{
     files: ['*.ts', '*.tsx', '*.js'],
-    parser: '@typescript-eslint/parser'
-  }]
-};
+    parser: '@typescript-eslint/parser',
+  }],
+}
