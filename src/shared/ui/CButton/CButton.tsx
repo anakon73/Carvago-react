439869ac35c903256f7a313e37cb31f38b3cdbb3
anakon73 <nazarkinah73@gmail.com>
2023-02-16 21:58:01 +0200

    feat(CButton): add style & size variants

--- src/shared/ui/CButton/CButton.tsx
+++ src/shared/ui/CButton/CButton.tsx
@@ -1,23 +1,46 @@
-import type { ButtonVariantType } from './types'
+import clsx from 'clsx'
+import type { ButtonVariant, ButtonSize } from './types'
 
 export interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode
-  variant?: ButtonVariantType
+  variant?: ButtonVariant
+  size?: ButtonSize
 }
 
-export const CButton = ({ children = 'Button', variant = 'light', ...props }: Props) => {
-  const classes: string[] = [
-    'px-4',
-    'py-2',
-    'text-white',
-    variant === 'primary'
-      ? 'bg-blue-200'
-      : variant === 'dark' ? 'bg-red-200' : 'bg-green-200',
-  ]
-
+export const CButton = ({ children, variant, size, ...props }: Props) => {
   return (
       <button
-          className={classes.join(' ')}
+          className={clsx(
+            'py-2 font-semibold flex',
+            [
+              size === 'square' && 'px-2',
+              size === 'sm' && 'px-4',
+              size === 'md' && 'px-12',
+              size === 'lg' && 'px-36',
+            ],
+            [
+              variant === 'primary' && [
+                'bg-blue-700 text-white rounded',
+                'disabled:bg-slate-300'
+              ],
+              variant === 'warning' && [
+                'bg-gradient-to-r from-yellow-600 to-orange-600 text-white rounded',
+                'disabled:from-orange-200 disabled:to-orange-200',
+                'hover:from-orange-700 hover:to-orange-700'
+              ],
+              variant === 'light' && [
+                'text-slate-500 rounded font-light',
+                'hover:bg-slate-100'
+              ],
+              variant === 'link' && [
+                'text-blue-400 font-light',
+              ],
+              variant === 'underline' && [
+                'text-blue-800',
+                'hover:underline hover:underline-offset-8'
+              ],
+            ]
+          )}
           {...props}
       >
           {children}
--- src/shared/ui/CButton/types.ts
+++ src/shared/ui/CButton/types.ts
@@ -1,3 +1,7 @@
-export type ButtonVariantType = typeof ButtonVariant[number]
+export type ButtonVariant = typeof ButtonVariant[number]
 
-export const ButtonVariant = ['primary', 'light', 'dark'] as const
+export const ButtonVariant = ['primary', 'warning', 'light', 'link','underline'] as const
+
+export type ButtonSize = typeof ButtonSize[number]
+
+export const ButtonSize = ['sm', 'md', 'lg', 'square']
