11cd85ee2a0a481a241651434dc77ca0bd052909
anakon73 <nazarkinah73@gmail.com>
2023-02-16 22:00:44 +0200

    docs(CButton): add controls for change style & size variants

--- src/shared/ui/CButton/CButton.stories.tsx
+++ src/shared/ui/CButton/CButton.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, Story } from '@storybook/react'
-import { ButtonVariant } from './types'
+import { ButtonVariant, ButtonSize } from './types'
 import { CButton, type CButtonProps } from '.'
+import {CakeIcon} from '@heroicons/react/24/solid'
 
 const meta: Meta<CButtonProps> = {
   title: 'Shared/UI/CButton',
@@ -9,6 +10,10 @@ const meta: Meta<CButtonProps> = {
     variant: {
       control: { type: 'select', options: ButtonVariant },
       defaultValue: 'primary',
+    },
+    size: {
+      control: { type: 'select', options: ButtonSize },
+      defaultValue: 'sm',
     },
     disabled: { control: { type: 'boolean' }, defaultValue: 'false' },
   },
@@ -19,5 +24,16 @@ export default meta
 export const Default: Story = props => (
     <CButton {...props} >
         Default
+    </CButton>
+)
+export const WithIcon: Story = props => (
+    <CButton {...props} >
+        <CakeIcon className='h-6 w-6'/>
+    </CButton>
+)
+export const WithIconAndText: Story = props => (
+    <CButton {...props} >
+        <CakeIcon className='h-6 w-6 mr-3'/>
+        <span>text</span>
     </CButton>
 )
