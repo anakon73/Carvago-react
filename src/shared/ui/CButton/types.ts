91453f3b8f1ddc0ce9d0ccb9c85c98745fb4d1c6
anakon73 <nazarkinah73@gmail.com>
2023-02-17 21:56:50 +0200

    style(CButton): reorder types

--- src/shared/ui/CButton/types.ts
+++ src/shared/ui/CButton/types.ts
@@ -1,7 +1,5 @@
-export type ButtonVariant = typeof ButtonVariant[number]
-
 export const ButtonVariant = ['primary', 'warning', 'light', 'link','underline'] as const
+export const ButtonSize = ['sm', 'md', 'lg', 'square'] as const
 
 export type ButtonSize = typeof ButtonSize[number]
-
-export const ButtonSize = ['sm', 'md', 'lg', 'square'] as const
+export type ButtonVariant = typeof ButtonVariant[number]
