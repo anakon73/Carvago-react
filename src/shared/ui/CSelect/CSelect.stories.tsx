39056586f17d322c5f52fe7461c447c049f50e5a
anakon73 <nazarkinah73@gmail.com>
2023-02-17 22:00:29 +0200

    docs(CSelect): describe stories

---
+++ src/shared/ui/CSelect/CSelect.stories.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import type { Meta, Story } from '@storybook/react'
+import { roundedVariant } from './types'
+import type { CSelectProps } from '.'
+import { CSelect } from '.'
+
+const years = [
+  2023,
+  2022,
+  2021,
+  2020,
+  2019,
+  2018,
+  2017,
+  2016,
+  2015,
+  2014,
+]
+
+const meta: Meta<CSelectProps<number>> = {
+  title: 'Shared/UI/CSelect',
+  component: CSelect,
+  argTypes: {
+    rounededSide: {
+      control: { type: 'select', options: roundedVariant },
+      defaultValue: 'noRounded',
+    },
+  },
+}
+
+export default meta
+
+export const Default: Story = (props) => {
+  const [year, setYear] = useState(0)
+
+  return (
+      <CSelect
+          itemsList={years}
+          onChange={(e: number) => setYear(e)}
+          value={year}
+          {...props}
+      />
+  )
+}
