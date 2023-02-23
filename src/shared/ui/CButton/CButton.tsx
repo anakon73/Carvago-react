9644476f1152d0744437930625e9dee51be7d9d3
anakon73 <nazarkinah73@gmail.com>
2023-02-23 18:58:46 +0200

    refactor(CButton): add attribute for class on component-tag & corrected classes

--- src/shared/ui/CButton/CButton.tsx
+++ src/shared/ui/CButton/CButton.tsx
@@ -1,50 +1,60 @@
 import clsx from 'clsx'
+import React from 'react'
 import type { ButtonSize, ButtonVariant } from './types'
 
 export interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode
   variant?: ButtonVariant
   size?: ButtonSize
+  classes?: string
 }
 
-export const CButton = ({ children, variant, size, ...props }: Props) => {
+export const CButton = React.forwardRef<HTMLButtonElement, Props>((
+  { children, variant, size, classes, ...props },
+  ref,
+) => {
   return (
       <button
           className={clsx(
-            'flex py-2 font-semibold',
+            classes,
+            'flex items-center font-sans',
             [
               size === 'square' && 'px-2',
               size === 'sm' && 'px-4',
               size === 'md' && 'px-12',
-              size === 'lg' && 'px-36',
+              size === 'lg' && 'px-24',
             ],
             [
               variant === 'primary' && [
-                'rounded bg-blue-700 text-white',
+                'rounded bg-blue-700 py-2 text-white',
                 'disabled:bg-slate-300',
               ],
               variant === 'warning' && [
-                'rounded bg-gradient-to-r from-yellow-600',
+                'rounded bg-gradient-to-r from-yellow-600 py-2',
                 'to-orange-600 text-white',
                 'disabled:from-orange-200 disabled:to-orange-200',
                 'hover:from-orange-700 hover:to-orange-700',
               ],
               variant === 'light' && [
-                'rounded font-light text-slate-500',
-                'hover:bg-slate-100',
+                'rounded py-1 font-normal text-slate-600',
+                'hover:bg-slate-100 hover:font-semibold',
               ],
               variant === 'link' && [
                 'font-light text-blue-400',
               ],
               variant === 'underline' && [
-                'text-blue-800',
-                'hover:underline hover:underline-offset-8',
+                'text-indigo-900 decoration-[3px]',
+                'hover:underline hover:underline-offset-[30px]',
+                'hover:text-blue-800',
               ],
             ],
           )}
+          ref={ref}
           {...props}
       >
           {children}
       </button>
   )
-}
+})
+
+CButton.displayName = 'CButton'
