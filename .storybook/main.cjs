e6c42b4e4613c244f36098959a67fcc6bb28e070
anakon73 <nazarkinah73@gmail.com>
2023-03-01 20:58:41 +0200

    chore(Storybook): alias import

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -14,5 +16,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, "../src/"),
+    };
+    return config;
   }
 }
