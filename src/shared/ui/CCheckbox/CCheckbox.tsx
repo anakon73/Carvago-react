4d7d2cc06f7563d4fcdede80e8f95921b4a50eeb
anakon73 <nazarkinah73@gmail.com>
2023-03-01 21:06:10 +0200

    feat(CCheckbox): add children prop

--- src/shared/ui/CCheckbox/CCheckbox.tsx
+++ src/shared/ui/CCheckbox/CCheckbox.tsx
@@ -2,20 +2,26 @@ import clsx from 'clsx'
 
 export interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   withOutline?: boolean
+  children?: React.ReactNode
 }
 
-export const CCheckbox = ({ withOutline, ...props }: Props) => {
+export const CCheckbox = ({ withOutline, children, ...props }: Props) => {
   return (
-      <input
-          className={clsx(
-            'h-5 w-5 cursor-pointer rounded-md border-blue-800',
-            'checked:ring-2 checked:ring-blue-300 checked:ring-offset-0',
-            'focus:ring-blue-300 focus:ring-offset-0',
-            [withOutline && 'hover:ring-4 hover:ring-blue-300'],
-          )}
-          type="checkbox"
-          {...props}
-      />
+      <div className='flex items-center gap-4'>
+          <input
+              className={clsx(
+                'h-5 w-5 cursor-pointer rounded-md border-blue-800',
+                'checked:ring-2 checked:ring-blue-300 checked:ring-offset-0',
+                'focus:ring-blue-300 focus:ring-offset-0',
+                [withOutline && 'hover:ring-4 hover:ring-blue-300'],
+              )}
+              type="checkbox"
+              {...props}
+          />
 
+          <label className='text-slate-900'>
+              {children}
+          </label>
+      </div>
   )
 }
