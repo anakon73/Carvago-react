dbb3e0b1f3292adcb22c8df62a29ec8c7b920804
anakon73 <nazarkinah73@gmail.com>
2023-03-13 19:43:42 +0200

    docs(CAutocomplete): rename object keys

--- src/shared/ui/CAutocomplete/CAutocomplete.stories.tsx
+++ src/shared/ui/CAutocomplete/CAutocomplete.stories.tsx
@@ -1,21 +1,34 @@
+/* eslint-disable react/no-multi-comp */
 import type { Meta, Story } from '@storybook/react'
-import { useState } from 'react'
+import { roundedVariant, variant } from './types'
 import { CAutocomplete, type CAutocompleteProps } from '.'
 
 const meta: Meta<CAutocompleteProps<any>> = {
   title: 'Shared/UI/CAutocomplete',
   component: CAutocomplete,
+  argTypes: {
+    rounededSide: {
+      control: { type: 'select', options: roundedVariant },
+    },
+    variant: {
+      control: { type: 'select', options: variant },
+    },
+  },
+  args: {
+    rounededSide: 'square',
+    variant: 'noBorder',
+  },
 }
 
 export default meta
 
 const people = [
-  { value: 1, label: 'Wade Cooper' },
-  { value: 2, label: 'Arlene Mccoy' },
-  { value: 3, label: 'Devon Webb' },
-  { value: 4, label: 'Tom Cook' },
-  { value: 5, label: 'Tanya Fox' },
-  { value: 6, label: 'Hellen Schmidt' },
+  { id: 1, value: 'Wade Cooper' },
+  { id: 2, value: 'Arlene Mccoy' },
+  { id: 3, value: 'Devon Webb' },
+  { id: 4, value: 'Tom Cook' },
+  { id: 5, value: 'Tanya Fox' },
+  { id: 6, value: 'Hellen Schmidt' },
 ]
 
 export const Default: Story = (props) => {
@@ -23,10 +36,35 @@ export const Default: Story = (props) => {
 
   return (
       <CAutocomplete
+          {...props}
           itemsList={people}
           onChange={setSelected}
           value={selected}
-          {...props}
       />
+  )
+}
+
+export const AutocompleteGroup: Story = (props) => {
+  const [selected1, setSelected1] = useState()
+  const [selected2, setSelected2] = useState()
+
+  return (
+      <div className='flex'>
+          <CAutocomplete
+              {...props}
+              itemsList={people}
+              onChange={setSelected1}
+              rounededSide='left'
+              value={selected1}
+          />
+
+          <CAutocomplete
+              {...props}
+              itemsList={people}
+              onChange={setSelected2}
+              rounededSide='right'
+              value={selected2}
+          />
+      </div>
   )
 }
